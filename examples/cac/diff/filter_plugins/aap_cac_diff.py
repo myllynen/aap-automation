3efd294e9cb6c1019587a31c559ed34547c4911f
Marko Myllynen <myllynen@redhat.com>
2026-08-05 15:57:16 +0300

    Improve aap_config_diff.yml, assume latest features of collections

--- examples/cac/diff/filter_plugins/aap_cac_diff.py
+++ examples/cac/diff/filter_plugins/aap_cac_diff.py
@@ -2,22 +2,56 @@
 #
 # pylint: disable=line-too-long,too-few-public-methods
 
-"""Filters to normalize AAP CaC data"""
+"""Filters to normalize and compare AAP configuration"""
 
 from ansible.utils.unsafe_proxy import wrap_var
 
 # May be used in infra.aap_configuration CaC but not exported or compared
-KEYS_TO_IGNORE = {'lookup_organization', 'password', 'roles', 'slug', 'token'}
+KEYS_TO_IGNORE = {'lookup_organization', 'password', 'slug', 'token'}
+
+# Default values to ignore to prevent noise in output
+DEFAULTS_TO_IGNORE = (False, "", 0, "0", [], {}, None)
 
 class FilterModule():
     """Filters module"""
     def filters(self):
         """Filter methods"""
         return {
+            'merge_configs': merge_configs,
             'normalize_brackets': normalize_brackets,
             'normalize_lists': normalize_lists,
-            'omit_deletions': omit_deletions
+            'omit_deletions': omit_deletions,
+            'omit_ignored': omit_ignored
         }
+
+
+
+def merge_configs(repo, live):
+    """Merge live config to repo config while pruning unmanaged entities"""
+    if not repo or not isinstance(repo, dict) or not isinstance(live, dict):
+        return repo
+
+    # Merge only entities present in repo
+    if all(isinstance(v, dict) for v in repo.values()):
+        return wrap_var({
+            k: merge_configs(v, live[k]) if k in live else v
+            for k, v in repo.items()
+        })
+
+    # Retain live operational defaults, overlay repo values
+    merged = dict(repo)
+    for k, v in repo.items():
+        if k in live and isinstance(v, dict) and isinstance(live[k], dict):
+            merged[k] = merge_configs(v, live[k])
+        else:
+            merged[k] = v
+
+    # Reduce diff noise by ignoring specified defaults
+    for k, live_val in live.items():
+        if k not in repo and live_val in DEFAULTS_TO_IGNORE:
+            merged[k] = live_val
+
+    return wrap_var(merged)
 
 # Handle infra.aap_configuration credential types injector syntax
 def normalize_brackets(data, in_injectors=False):
@@ -36,7 +70,7 @@ def normalize_brackets(data, in_injectors=False):
             return wrap_var(fixed)
         return fixed
 
-    return data
+    return wrap_var(data)
 
 def normalize_lists(data):
     """Normalize lists in AAP CaC"""
@@ -57,7 +91,7 @@ def normalize_lists(data):
                 }
         return [normalize_lists(item) for item in data]
 
-    return data
+    return wrap_var(data)
 
 def omit_deletions(data, is_root=True):
     """Remove AAP CaC items with absent state"""
@@ -76,4 +110,24 @@ def omit_deletions(data, is_root=True):
         else:
             cleaned[k] = v
 
-    return cleaned
+    return wrap_var(cleaned)
+
+def omit_ignored(data, ignore_map):
+    """Remove ignored entities from data"""
+    if not isinstance(data, dict) or not ignore_map:
+        return data
+
+    cleaned = {}
+    for key, val in data.items():
+        rule = ignore_map.get(key)
+
+        if rule is None:
+            cleaned[key] = val
+        elif rule == '*' or rule is True:
+            continue
+        elif isinstance(rule, list) and isinstance(val, dict):
+            sub_dict = {k: v for k, v in val.items() if k not in rule}
+            if sub_dict:
+                cleaned[key] = sub_dict
+
+    return wrap_var(cleaned)
